87db8eb1db30ac5897d238339c3480b26048d9de
lahphim <lahphim@gmail.com>
2017-08-15 18:18:12 +0700

    init bulma

---
+++ app/js/main.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Lahphim on 8/15/2017 AD.
+ */
---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+var gulp = require('gulp');
+var cache = require('gulp-cached');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var autoprefixer = require('gulp-autoprefixer');
+var minify = require('gulp-minifier');
+
+gulp.task('third-party', function() {
+  try {
+
+    gulp.src(['./node_modules/bulma/**/*.css', './node_modules/bulma/**/*.sass'])
+      .pipe(cache('bulma'))
+      .pipe(gulp.dest('./app/lib/bulma'));
+  }
+  catch (e) {
+    return -1;
+  }
+  return 0;
+});
+
+gulp.task('styles', function() {
+  gulp.src('./app/lib/bulma/bulma.sass')
+    .pipe(sass())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('./app/css'));
+
+  gulp.src('./app/scss/styles.scss')
+    .pipe(sass())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('./app/css'))
+    .pipe(browserSync.reload({stream: true}));
+});
+
+// gulp.task('minifier', function() {
+//   gulp.src(['./app/css/*.*', './app/js/*.*']).pipe(minify({
+//     minify: true,
+//     collapseWhitespace: true,
+//     conservativeCollapse: true,
+//     minifyJS: true,
+//     minifyCSS: true,
+//     getKeptComment: function (content, filePath) {
+//       var m = content.match(/\/\*![\s\S]*?\*\//img);
+//       return m && m.join('\n') + '\n' || '';
+//     }
+//   })).pipe(gulp.dest('./dist'));
+// });
+
+gulp.task('serve', function(){
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
+
+  gulp.watch('./app/scss/*.scss', ['styles']);
+  gulp.watch('./**/*.html').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['styles', 'serve']);
