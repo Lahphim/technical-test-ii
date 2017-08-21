ff3e0490dd2b18dc9ff639e321fe25aafb6618c4
lahphim <lahphim@gmail.com>
2017-08-21 12:04:58 +0700

    update sidebar's toggle action

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,15 @@
 /**
  * Created by Lahphim on 8/15/2017 AD.
  */
+
+$(function() {
+  $("#toggle-sidebar-btn")
+    .click(function() {
+      $(document.body).addClass("aside-show");
+    });
+
+  $(".aside-overlay")
+    .click(function(){
+      $(document.body).removeClass("aside-show");
+    });
+});
